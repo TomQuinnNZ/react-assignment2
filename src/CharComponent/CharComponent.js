ade102a3403214c24bdd0e0ddf51869a69a965e5
Tom Quinn <tomquinn@hotmail.co.nz>
2019-05-15 00:14:51 +1200

    implement basic CharComponent + styling

---
+++ src/CharComponent/CharComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './CharComponent.css';
+
+const CharComponent = (props) => {
+    return (
+        <p className="CharComponent" onClick={props.click}>
+            {props.character}
+        </p>
+    );
+}
